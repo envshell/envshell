e23e5e561de72a159a8167c81227a5a0e99f2973
RobBurrus <robert.burrus@uky.edu>
2014-04-02 16:24:53 -0400

    Parser start

---
+++ envShell/envShell/Parser.cpp
@@ -0,0 +1,6 @@
+#include "Parser.h"
+
+Parser::Parser(string commandString) {
+	myString = commandString;
+}
+
--- envShell/envShell/Parser.h
+++ envShell/envShell/Parser.h
@@ -2,3 +2,19 @@
 #define PARSER_H
 
 #endif
+#include<string>
+#include"Token.h"
+using namespace std;
+
+class Parser{
+public:
+	Parser(string commandString);
+	//void parse();
+
+
+private:
+	string myString;
+
+
+
+};
