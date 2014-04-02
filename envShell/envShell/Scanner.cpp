3c41e46090dad518fb36796f5535dd14d62fcb7b
adhe223 <adhe223@g.uky.edu>
2014-04-02 16:41:24 -0400

    Added Doxumentation

--- envShell/envShell/Scanner.cpp
+++ envShell/envShell/Scanner.cpp
@@ -3,10 +3,17 @@
 #include <string>
 using namespace std;
 
-Scanner::Scanner() {
-
-}
+Scanner::Scanner() {}
 
 vector<Token> Scanner::scan(string inCommand) {
-
+	char * command = new char[256]();
+	command = inCommand.c_str();
+	char * pch;
+	printf("Splitting string \"%s\" into tokens:\n", str);
+	pch = strtok(str, " ,.-");
+	while (pch != NULL)
+	{
+		printf("%s\n", pch);
+		pch = strtok(NULL, " ,.-");
+	}
 }
