1d8144bf92659636952a54fc544f1f985158eef6
adhe223 <adhe223@g.uky.edu>
2014-04-02 16:26:30 -0400

    Scanner Class oprototyped

---
+++ envShell/envShell/Scanner.cpp
@@ -0,0 +1,12 @@
+#include "Scanner.h"
+#include <vector>
+#include <string>
+using namespace std;
+
+Scanner::Scanner() {
+
+}
+
+vector<Token> Scanner::scan(string inCommand) {
+
+}
--- envShell/envShell/Scanner.h
+++ envShell/envShell/Scanner.h
@@ -1,4 +1,16 @@
 #ifndef SCANNER_H
 #define SCANNER_H
+#include <string>
+#include <vector>
+#include "Token.h"
+
+class Scanner {
+public:
+	Scanner();
+	vector<Token> scan(string inCommand);
+
+private:
+	vector<Token> tokens;
+};
 
 #endif
