a201e3613a7b85c79b60a4d00add6258b5e606ac
RobBurrus <robert.burrus@uky.edu>
2014-04-02 17:06:29 -0400

    Parser continued

--- envShell/envShell/Parser.cpp
+++ envShell/envShell/Parser.cpp
@@ -1,6 +1,35 @@
 #include "Parser.h"
+#include "Scanner.h"
 
 Parser::Parser(string commandString) {
 	myString = commandString;
 }
 
+string Parser::getCommandString(){
+	return myString;
+}
+
+void Parser::parse(){
+	Scanner s;
+	myTokens = s.scan(myString);
+	doSomething();
+}
+
+void Parser::doSomething(){
+	myCommand = myTokens[0].getValue;
+	//store arguments until hit "<" or EOL
+	int i = 1;
+	while(myTokens[i].getValue() != "<" && myTokens[i].getValue() != "EOL"){
+		myArguments.push_back(myTokens[i].getValue());
+		i++;
+	}
+	//check to see if there is an input file
+	if(myTokens[i].getValue() == "<"){
+		myInfile = myTokens[i++].getValue();
+	}
+	//check to see if there is an output file
+	if(myTokens[i].getValue() == ">"){
+		myOutfile = myTokens[i++].getValue();
+	}
+
+}
--- envShell/envShell/Parser.h
+++ envShell/envShell/Parser.h
@@ -1,20 +1,30 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-#endif
+
 #include<string>
+#include<vector>
 #include"Token.h"
 using namespace std;
 
 class Parser{
 public:
 	Parser(string commandString);
-	//void parse();
+	void parse();
 
+	string getCommandString();
+
+	void doSomething();
 
 private:
 	string myString;
-
+	vector<Token> myTokens;
+	string myCommand;
+	vector<string> myArguments;
+	string myInfile;
+	string myOutfile;
 
 
 };
+
+#endif
